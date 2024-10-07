a3ac31b70b8f7c9b443376010124513ae5f9a005
mduzh <duzh.maxim@gmail.com>
2024-10-07 20:59:06 +0300

    done

--- src/BubbleSortCount.java
+++ src/BubbleSortCount.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class BubbleSort {
+public class BubbleSortCount {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,17 +12,18 @@ public class BubbleSort {
             arr[el] = scanner.nextInt();
         }
 
-
+        int res = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int t = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = t;
+                    res += 1;
                 }
 
             }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(res);
     }
 }
