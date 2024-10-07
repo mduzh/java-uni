ebfc72e25e64f0f003cb12fd31fd8114032d710a
mduzh <duzh.maxim@gmail.com>
2024-10-07 20:41:22 +0300

    added

---
+++ src/BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int l = scanner.nextInt();
+
+        int[] arr = new int[l];
+
+        for (int el = 0; el < arr.length; el++) {
+            arr[el] = scanner.nextInt();
+        }
+
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                    res += 1;
+                }
+
+            }
+        }
+        System.out.println(res);
+    }
+}
