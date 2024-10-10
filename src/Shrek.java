47067538a68f5262d5c6219ea223090fa1d43c59
mduzh <duzh.maxim@gmail.com>
2024-10-10 22:09:17 +0300

    done

---
+++ src/Shrek.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Shrek {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int l = scanner.nextInt();
+
+        int[] arr = new int[l];
+
+        for (int el = 0; el < arr.length; el++) {
+            arr[el] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+
+        int[] lst = new int[]{arr.length / 2};
+        for(int k = (arr.length / 2) + 1; k < arr.length; k++) {
+        }
+    }
+}
---
+++ src/hockey.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class hockey {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int l = scanner.nextInt();
+
+        long res = factorial(l) / factorial(l - 3);
+        System.out.println(res);
+    }
+
+    public static long factorial(long l) {
+        long N = 1;
+        for(long j = 1; j <= l; j++) {
+            N = N * j;
+        }
+        return N;
+    }
+}
